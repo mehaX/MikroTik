0ff5afa6aab91dcb75d0a3bdd841992d6348751b
Melos Hasanaj <melos.hasanaj@gmail.com>
2020-02-03 14:53:07 +0100

    CORS > enabled for public (*)

--- MikroTik.WebApi/Startup.cs
+++ MikroTik.WebApi/Startup.cs
@@ -71,14 +71,14 @@ namespace MikroTik.WebApi
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-
-                app.UseCors("CorsPolicy");
             }
             else
             {
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            app.UseCors("CorsPolicy");
 
             app.UseSignalR(routes =>
             {
